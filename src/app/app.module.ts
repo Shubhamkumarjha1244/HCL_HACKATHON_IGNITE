e55301028d5984bf2474a0d0368b69e38a2ee56d
Shubhamkumarjha1244 <jha.kumar.shubham1244@gmail.com>
2025-12-19 16:32:34 +0530

    1st commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './auth/login/login.component';
+import { PatientDashboardComponent } from './dashboard/patient-dashboard/patient-dashboard.component';
+import { GoalTrackerComponent } from './goals/goal-tracker/goal-tracker.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    PatientDashboardComponent,
+    GoalTrackerComponent
   ],
   imports: [
     BrowserModule,
